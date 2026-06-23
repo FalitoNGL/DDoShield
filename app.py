c512457f85a9be838b160ec6b900631b8c8c71e0
Falito Eriano <falitoeriano17@gmail.com>
2026-06-23 14:22:20 +0700

    kelompok 3

--- app.py
+++ app.py
@@ -1007,5 +1007,5 @@ def explain(flow_idx):
     })
 
 if __name__ == '__main__':
-    print("[DDoShield] Starting development server on http://127.0.0.1:5050")
-    app.run(debug=True, port=5050)
+    print("[DDoShield] Starting development server on http://127.0.0.1:5000")
+    app.run(debug=True, port=5000)
